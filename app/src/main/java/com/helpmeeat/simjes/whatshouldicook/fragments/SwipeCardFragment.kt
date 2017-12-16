de5a56467d893109e0eda48b16202ca48dc8618e
Simon Jespersen <simjes91@me.com>
2017-12-17 00:50:48 +0100

    impl some binding

--- app/src/main/java/com/helpmeeat/simjes/whatshouldicook/fragments/SwipeCardFragment.kt
+++ app/src/main/java/com/helpmeeat/simjes/whatshouldicook/fragments/SwipeCardFragment.kt
@@ -1,5 +1,6 @@
 package com.helpmeeat.simjes.whatshouldicook.fragments
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.os.Handler
@@ -9,7 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.helpmeeat.simjes.whatshouldicook.R
 import com.helpmeeat.simjes.whatshouldicook.adapters.RecipeCardAdapter
-import com.helpmeeat.simjes.whatshouldicook.viewmodels.RecipeViewModel
+import com.helpmeeat.simjes.whatshouldicook.viewmodels.RecipeListViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
 import com.yuyakaido.android.cardstackview.SwipeDirection
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -17,7 +18,7 @@ import com.yuyakaido.android.cardstackview.CardStackView
 
 
 class SwipeCardFragment : Fragment() {
-
+    var recipeListViewModel: RecipeListViewModel? = null
     var adapter: RecipeCardAdapter? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -27,29 +28,19 @@ class SwipeCardFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        recipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel::class.java!!)
+        adapter = RecipeCardAdapter(this.context!!)
+
+        val tempAdapter = RecipeCardAdapter(this.context!!)
+        tempAdapter.addAll(recipeListViewModel!!.recipes)
+
+        adapter = tempAdapter
+        fragment_card_stack_view.setAdapter(adapter)
+        fragment_card_stack_view.visibility = View.VISIBLE
+        fragment_progress_bar.visibility = View.GONE
+
         setup()
-        reload()
-    }
-
-    private fun createRecipes(): List<RecipeViewModel> {
-        val recipes = ArrayList<RecipeViewModel>()
-        recipes.add(RecipeViewModel(0, "Yasaka Shrine", "Kyoto", "https://source.unsplash.com/Xq1ntWruZQI/600x800"))
-        recipes.add(RecipeViewModel(1, "Fushimi Inari Shrine", "Kyoto", "https://source.unsplash.com/NYyCqdBOKwc/600x800"))
-        recipes.add(RecipeViewModel(2, "Bamboo Forest", "Kyoto", "https://source.unsplash.com/buF62ewDLcQ/600x800"))
-        recipes.add(RecipeViewModel(3, "Brooklyn Bridge", "New York", "https://source.unsplash.com/THozNzxEP3g/600x800"))
-        recipes.add(RecipeViewModel(4, "Empire State Building", "New York", "https://source.unsplash.com/USrZRcRS2Lw/600x800"))
-        recipes.add(RecipeViewModel(5, "The statue of Liberty", "New York", "https://source.unsplash.com/PeFk7fzxTdk/600x800"))
-        recipes.add(RecipeViewModel(6, "Louvre Museum", "Paris", "https://source.unsplash.com/LrMWHKqilUw/600x800"))
-        recipes.add(RecipeViewModel(7, "Eiffel Tower", "Paris", "https://source.unsplash.com/HN-5Z6AmxrM/600x800"))
-        recipes.add(RecipeViewModel(8, "Big Ben", "London", "https://source.unsplash.com/CdVAUADdqEc/600x800"))
-        recipes.add(RecipeViewModel(9, "Great Wall of China", "China", "https://source.unsplash.com/AWh9C-QjhE4/600x800"))
-        return recipes
-    }
-
-    private fun createRecipeCardAdapter(): RecipeCardAdapter {
-        val adapter = RecipeCardAdapter(this.context!!)
-        adapter.addAll(createRecipes())
-        return adapter
+        //reload()
     }
 
     private fun setup() {
@@ -79,17 +70,6 @@ class SwipeCardFragment : Fragment() {
                 Log.d("CardStackView", "onCardClicked: " + index)
             }
         })
-    }
-
-    private fun reload() {
-        fragment_card_stack_view.visibility = View.GONE
-        fragment_progress_bar.visibility = View.VISIBLE
-        Handler().postDelayed({
-            adapter = createRecipeCardAdapter()
-            fragment_card_stack_view.setAdapter(adapter)
-            fragment_card_stack_view.visibility = View.VISIBLE
-            fragment_progress_bar.visibility = View.GONE
-        }, 1000)
     }
 
     /*private fun paginate() {
