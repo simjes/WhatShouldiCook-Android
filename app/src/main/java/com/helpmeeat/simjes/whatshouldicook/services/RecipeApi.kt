24fe8f3e2791ab38a57a9e867dfeb52863516fb1
Simon Jespersen <simjes91@me.com>
2018-01-10 23:03:47 +0100

    async await instead of coroutines

--- app/src/main/java/com/helpmeeat/simjes/whatshouldicook/services/RecipeApi.kt
+++ app/src/main/java/com/helpmeeat/simjes/whatshouldicook/services/RecipeApi.kt
@@ -1,7 +1,6 @@
 package com.helpmeeat.simjes.whatshouldicook.services
 
 import com.helpmeeat.simjes.whatshouldicook.models.Recipe
-import com.helpmeeat.simjes.whatshouldicook.models.RecipeResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
