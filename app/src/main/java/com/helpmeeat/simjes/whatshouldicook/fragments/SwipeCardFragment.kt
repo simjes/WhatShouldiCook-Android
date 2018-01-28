a8d38b4a45d7228504addc2b82f6587e4dc750a2
Simon Jespersen <simjes91@me.com>
2018-01-28 14:09:45 +0100

    recyclerview instead of card stack

--- app/src/main/java/com/helpmeeat/simjes/whatshouldicook/fragments/SwipeCardFragment.kt
+++ app/src/main/java/com/helpmeeat/simjes/whatshouldicook/fragments/SwipeCardFragment.kt
@@ -1,24 +1,28 @@
 package com.helpmeeat.simjes.whatshouldicook.fragments
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.databinding.ObservableList
 import android.support.v4.app.Fragment
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.nitrico.lastadapter.LastAdapter
+import com.helpmeeat.simjes.whatshouldicook.BR
 import com.helpmeeat.simjes.whatshouldicook.R
-import com.helpmeeat.simjes.whatshouldicook.adapters.RecipeCardAdapter
+import com.helpmeeat.simjes.whatshouldicook.models.Recipe
 import com.helpmeeat.simjes.whatshouldicook.viewmodels.RecipeListViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
-import com.yuyakaido.android.cardstackview.SwipeDirection
-import com.yuyakaido.android.cardstackview.CardStackView
 
 class SwipeCardFragment : Fragment() {
     lateinit var recipeListViewModel: RecipeListViewModel
-    lateinit var adapter: RecipeCardAdapter
-    var counter = 0
+    lateinit var adapter: LastAdapter
+    lateinit var recipeList: MutableList<Recipe>
+    var initalLoad = true
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -28,48 +32,24 @@ class SwipeCardFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         recipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel::class.java)
-        adapter = RecipeCardAdapter(this.context!!)
+
+        recipeList = mutableListOf()
+
+
+        recipe_list_view.layoutManager = LinearLayoutManager(this.context)
+        adapter = LastAdapter(recipeList, BR.item)
+                .map<Recipe>(R.layout.recipe_card)
+                .into(recipe_list_view)
 
         recipeListViewModel.recipes.observe(this, Observer { recipeList ->
-            adapter.clear()
-            adapter.addAll(recipeList)
+            this.recipeList.addAll(recipeList!!)
             adapter.notifyDataSetChanged()
-        })
-        fragment_card_stack_view.setAdapter(adapter)
-        fragment_card_stack_view.visibility = View.VISIBLE
-        fragment_progress_bar.visibility = View.GONE
 
-        setup()
-    }
-
-    private fun setup() {
-        fragment_card_stack_view.setCardEventListener(object: CardStackView.CardEventListener {
-            override fun onCardDragging(percentX: Float, percentY: Float) {
-                Log.d("CardStackView", "onCardDragging")
-            }
-
-            override fun onCardSwiped(direction: SwipeDirection) {
-                recipeListViewModel.removeRecipe()
-
-                counter++
-                if(counter % 4 == 0) {
-                    recipeListViewModel.loadMoreRecipes()
-                }
-
-                Log.d("CardStackView", "onCardSwiped: " + direction.toString())
-                Log.d("CardStackView", "topIndex: " + fragment_card_stack_view.topIndex)
-            }
-
-            override fun onCardReversed() {
-                Log.d("CardStackView", "onCardReversed")
-            }
-
-            override fun onCardMovedToOrigin() {
-                Log.d("CardStackView", "onCardMovedToOrigin")
-            }
-
-            override fun onCardClicked(index: Int) {
-                Log.d("CardStackView", "onCardClicked: " + index)
+            if (this.recipeList.count() != 0 && initalLoad) {
+                fragment_progress_bar.visibility = View.GONE
+                recipe_list_view.visibility = View.VISIBLE
+                initalLoad = false
+                
             }
         })
     }
